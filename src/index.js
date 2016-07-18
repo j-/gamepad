a3a3c9eeedcac78207a4ba4e001ad90bd04a8b4e
j- <j@skeoh.com>
2016-07-18 21:12:20 +1000

    Add Redux store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import { createStore } from 'redux';
+import reducer from './reducer';
+
+const store = createStore(reducer);
+
 window.addEventListener('gamepadconnected', (e) => {
 	console.group('Gamepad connected');
 	console.log(e);
