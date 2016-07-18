bc718df7f4a80b6a02c89b1ff20c1eb6a70bacf9
j- <j@skeoh.com>
2016-07-18 23:49:30 +1000

    Add gamepad reducer test

---
+++ src/reducer/gamepad.test.js
@@ -0,0 +1,42 @@
+import test from 'ava';
+import { createStore } from 'redux';
+import reducer from './gamepad';
+
+import {
+	gamepadConnected,
+} from './actions';
+
+test('Can use reducer to create store', (t) => {
+	t.plan(1);
+	const store = createStore(reducer);
+	t.truthy(store, 'Store was created');
+});
+
+test('Can create a new gamepad', (t) => {
+	t.plan(1);
+	const gamepad = {
+		id: 'Xbox 360 Controller (XInput STANDARD GAMEPAD)',
+		index: 0,
+		connected: true,
+		timestamp: 0,
+		mapping: 'standard',
+		axes: [0.0000152587890625, -0.0000152587890625, 0.0000152587890625, -0.0000152587890625],
+		buttons: [],
+	};
+	const action = gamepadConnected(gamepad);
+	const actual = reducer(0)(undefined, action);
+	const expected = {
+		id: 'Xbox 360 Controller (XInput STANDARD GAMEPAD)',
+		index: 0,
+		connected: true,
+		timestamp: 0,
+		mapping: 'standard',
+		axes: [0.0000152587890625, -0.0000152587890625, 0.0000152587890625, -0.0000152587890625],
+		buttons: [],
+	};
+	t.deepEqual(
+		actual,
+		expected,
+		'Gamepad matches expected shape'
+	);
+});
