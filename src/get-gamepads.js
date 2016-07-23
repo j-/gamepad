75378257fb17dbde3fe12dbe5cd73e9ba65c1e82
j- <j@skeoh.com>
2016-07-23 18:05:29 +1000

    Implement gamepad reducer in root reducer

--- src/get-gamepads.js
+++ src/get-gamepads.js
@@ -1,7 +1,12 @@
 let getGamepads;
 
 if (typeof navigator.getGamepads === 'function') {
-	getGamepads = () => navigator.getGamepads();
+	getGamepads = () => (
+		// Convert iterable to array
+		[...navigator.getGamepads()]
+			// Gamepads may be `undefined` despite `length` of iterable
+			.filter(Boolean)
+	);
 } else {
 	getGamepads = () => {
 		throw new Error('Unable to find any gamepads');
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,5 +1,23 @@
-export default (state = null, action) => {
+import reducer from './gamepad';
+
+import {
+	GAMEPADS_UPDATE,
+} from './types';
+
+export default (state = {}, action) => {
 	switch (action.type) {
+		case GAMEPADS_UPDATE:
+			const result = {};
+
+			[...action.gamepads].forEach((gamepad) => {
+				const index = gamepad.index;
+				result[index] = reducer(index)(
+					state[index],
+					action
+				);
+			});
+
+			return result;
 		default:
 			return state;
 	}
