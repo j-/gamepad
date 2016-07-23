8677898f1df37338b98381974c94e7b020ea0d40
j- <j@skeoh.com>
2016-07-23 16:50:55 +1000

    Test gamepad updating

--- src/reducer/gamepad/gamepad.test.js
+++ src/reducer/gamepad/gamepad.test.js
@@ -4,6 +4,7 @@ import reducer from './';
 
 import {
 	gamepadConnected,
+	gamepadUpdate,
 } from '../actions';
 
 test('Can use reducer to create store', (t) => {
@@ -38,5 +39,77 @@ test('Can create a new gamepad', (t) => {
 		actual,
 		expected,
 		'Gamepad matches expected shape'
+	);
+});
+
+test('Can update a gamepad', (t) => {
+	t.plan(1);
+	const initial = {
+		id: 'Xbox 360 Controller (XInput STANDARD GAMEPAD)',
+		index: 0,
+		connected: true,
+		timestamp: 0,
+		mapping: 'standard',
+		axes: [0.0000152587890625, -0.0000152587890625, 0.0000152587890625, -0.0000152587890625],
+		buttons: [],
+	};
+	const updated = {
+		id: 'Xbox 360 Controller (XInput STANDARD GAMEPAD)',
+		index: 0,
+		connected: true,
+		timestamp: 100,
+		mapping: 'standard',
+		axes: [0.1525878906250000, -0.1525878906250000, 0.1525878906250000, -0.1525878906250000],
+		buttons: [],
+	};
+	const action = gamepadUpdate(updated);
+	t.deepEqual(
+		reducer(0)(initial, action),
+		{
+			id: 'Xbox 360 Controller (XInput STANDARD GAMEPAD)',
+			index: 0,
+			connected: true,
+			timestamp: 100,
+			mapping: 'standard',
+			axes: [0.1525878906250000, -0.1525878906250000, 0.1525878906250000, -0.1525878906250000],
+			buttons: [],
+		},
+		'Gamepad matches expected shape'
+	);
+});
+
+test('Ignores updates for other gamepads', (t) => {
+	t.plan(1);
+	const initial = {
+		id: 'Xbox 360 Controller (XInput STANDARD GAMEPAD)',
+		index: 0,
+		connected: true,
+		timestamp: 0,
+		mapping: 'standard',
+		axes: [0.0000152587890625, -0.0000152587890625, 0.0000152587890625, -0.0000152587890625],
+		buttons: [],
+	};
+	const updated = {
+		id: 'Xbox 360 Controller (XInput STANDARD GAMEPAD)',
+		index: 1, // Different index
+		connected: true,
+		timestamp: 100,
+		mapping: 'standard',
+		axes: [0.1525878906250000, -0.1525878906250000, 0.1525878906250000, -0.1525878906250000],
+		buttons: [],
+	};
+	const action = gamepadUpdate(updated);
+	t.deepEqual(
+		reducer(0)(initial, action),
+		{
+			id: 'Xbox 360 Controller (XInput STANDARD GAMEPAD)',
+			index: 0,
+			connected: true,
+			timestamp: 0,
+			mapping: 'standard',
+			axes: [0.0000152587890625, -0.0000152587890625, 0.0000152587890625, -0.0000152587890625],
+			buttons: [],
+		},
+		'Gamepad shape has not changed'
 	);
 });
