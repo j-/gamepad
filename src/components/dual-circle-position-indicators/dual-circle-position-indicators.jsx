ea52ca0cf01a164b66c0318544fa3f28d43b0e2f
j- <j@skeoh.com>
2016-07-23 19:46:01 +1000

    Do not assume component is a div

--- src/components/dual-circle-position-indicators/dual-circle-position-indicators.jsx
+++ src/components/dual-circle-position-indicators/dual-circle-position-indicators.jsx
@@ -3,12 +3,18 @@ import CirclePositionIndicator from '../circle-position-indicator';
 
 import {
 	dualCirclePositionIndicators,
+	leftCirclePositionIndicator,
+	rightCirclePositionIndicator,
 } from './styles';
 
 const DualCirclePositionIndicators = ({ x1, y1, x2, y2 }) => (
 	<div className={ dualCirclePositionIndicators }>
-		<CirclePositionIndicator x={ x1 } y={ y1 } />
-		<CirclePositionIndicator x={ x2 } y={ y2 } />
+		<div className={ leftCirclePositionIndicator }>
+			<CirclePositionIndicator x={ x1 } y={ y1 } />
+		</div>
+		<div className={ rightCirclePositionIndicator }>
+			<CirclePositionIndicator x={ x2 } y={ y2 } />
+		</div>
 	</div>
 );
 
