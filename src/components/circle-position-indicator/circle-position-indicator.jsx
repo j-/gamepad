a9f878a84b72c21735123c6da11cd9bda38558ed
j- <j@skeoh.com>
2016-07-23 20:03:08 +1000

    Use canvas to render circle position indicator component

--- src/components/circle-position-indicator/circle-position-indicator.jsx
+++ src/components/circle-position-indicator/circle-position-indicator.jsx
@@ -1,29 +1,77 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import {
 	positionIndicatorInner,
 	positionIndicatorOuter,
 } from './styles';
 
-const Position = ({ x, y }) => (
-	<div
-		className={ positionIndicatorInner }
-		style={{
-			left: ((x + 1) * 50) + '%',
-			top: ((y + 1) * 50) + '%',
-		}}
-	/>
-);
+class CirclePositionIndicator extends Component {
+	componentDidMount () {
+		const { size } = this.props;
+		const canvas = document.createElement('canvas');
+		canvas.width = size;
+		canvas.height = size;
+		const context = canvas.getContext('2d');
+		this.canvas = canvas;
+		this.context = context;
+	}
 
-const CirclePositionIndicator = ({ x, y }) => (
-	<div className={ positionIndicatorOuter }>
-		<Position x={ x } y={ y } />
-	</div>
-);
+	componentWillUnmount () {
+		this.canvas = null;
+		this.context = null;
+	}
+
+	calculateLeftPosition (x, size) {
+		return (x + 1) * (size / 2);
+	}
+
+	calculateTopPosition (y, size) {
+		return (y + 1) * (size / 2);
+	}
+
+	drawIndicator () {
+		const { context } = this;
+		const { size, indicatorSize, x, y } = this.props;
+		// Calculate position of indicator
+		const left = this.calculateLeftPosition(x, size);
+		const top = this.calculateTopPosition(y, size);
+		// Remove previous indicator
+		context.clearRect(0, 0, size, size);
+		// Draw indicator
+		context.fillRect(
+			left - (indicatorSize / 2),
+			top - (indicatorSize / 2),
+			indicatorSize,
+			indicatorSize
+		);
+	}
+
+	appendCanvas () {
+		this.refs.outer.appendChild(this.canvas);
+	}
+
+	render () {
+		const element = (
+			<div className={ positionIndicatorOuter } ref="outer" />
+		);
+		if (this.canvas && this.refs.outer) {
+			this.drawIndicator();
+			this.appendCanvas();
+		}
+		return element;
+	}
+}
 
 CirclePositionIndicator.propTypes = {
+	size: PropTypes.number,
+	indicatorSize: PropTypes.number,
 	x: PropTypes.number.isRequired,
 	y: PropTypes.number.isRequired,
+};
+
+CirclePositionIndicator.defaultProps = {
+	size: 100,
+	indicatorSize: 4,
 };
 
 export default CirclePositionIndicator;
