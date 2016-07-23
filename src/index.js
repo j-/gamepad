d299e5677652116b8af87c3668fdf8048ee5d260
j- <j@skeoh.com>
2016-07-23 17:21:00 +1000

    Dispatch gamepad actions to store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,54 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
 
+import {
+	gamepadConnected,
+	gamepadDisconnected,
+	gamepadUpdate,
+} from './reducer/actions';
+
 const store = createStore(reducer);
 
-window.addEventListener('gamepadconnected', (e) => {
-	console.group('Gamepad connected');
-	console.log(e);
-	console.groupEnd();
+window.addEventListener('gamepadconnected', function (e) {
+	let gamepad = e.gamepad;
+
+	console.info(
+		'Gamepad connected',
+		`${gamepad.id} (${gamepad.index})`
+	);
+
+	// Notify store that a new gamepad has been connected
+	store.dispatch(
+		gamepadConnected(gamepad)
+	);
+
+	// Start watching gamepad for changes
+	let timer = setInterval(() => {
+		store.dispatch(
+			gamepadUpdate(gamepad)
+		);
+	}, 10);
+
+	// Start watching gamepad for a disconnection
+	window.addEventListener('gamepaddisconnected', function handler (e) {
+		if (e.gamepad.index !== gamepad.index) {
+			// Ignore other gamepads being disconnected
+		}
+
+		console.info(
+			'Gamepad disconnected',
+			`${gamepad.id} (${gamepad.index})`
+		);
+
+		// Notify the store that this gamepad was disconnected
+		store.dispatch(
+			gamepadDisconnected(gamepad)
+		);
+
+		// This gamepad was disconnected. Clean up.
+		clearInterval(timer);
+		timer = null;
+		gamepad = null;
+		window.removeEventListener('gamepaddisconnected', handler);
+	});
 });
