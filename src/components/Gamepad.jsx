759b063d8913cd0d01f0beb318c4f87b3bf97bd8
j- <j@skeoh.com>
2017-03-12 23:55:52 +1100

    Add some output to the gamepad component

--- src/components/Gamepad.jsx
+++ src/components/Gamepad.jsx
@@ -2,7 +2,30 @@ import React from 'react';
 
 const Gamepad = ({ gamepad }) => (
 	<div>
-		{ gamepad.id }
+		<div>
+			<strong>Index</strong><br />
+			<span>{ JSON.stringify(gamepad.index) }</span>
+		</div>
+		<div>
+			<strong>ID</strong><br />
+			<span>{ JSON.stringify(gamepad.id) }</span>
+		</div>
+		<div>
+			<strong>Mapping</strong><br />
+			<span>{ JSON.stringify(gamepad.mapping) }</span>
+		</div>
+		<div>
+			<strong>Connected</strong><br />
+			<span>{ JSON.stringify(gamepad.connected) }</span>
+		</div>
+		<div>
+			<strong>Buttons</strong><br />
+			<span>{ JSON.stringify(gamepad.buttons) }</span>
+		</div>
+		<div>
+			<strong>Axes</strong><br />
+			<span>{ JSON.stringify(gamepad.axes) }</span>
+		</div>
 	</div>
 );
 
