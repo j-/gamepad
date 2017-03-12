66a0e2be7e81da5e474ded6eb9178c4815d8cfdd
j- <j@skeoh.com>
2017-03-12 23:31:00 +1100

    Add Webpack build

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const DefinePlugin = webpack.DefinePlugin;
+const HTMLPlugin = require('html-webpack-plugin');
+
+const resolve = (relative) => path.resolve(__dirname, relative);
+
+module.exports = {
+	entry: resolve('./src/index.jsx'),
+	output: {
+		path: resolve('./dist'),
+		filename: 'bundle.js',
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				use: 'babel-loader',
+				include: [
+					resolve('./src'),
+				],
+				test: /\.jsx?$/,
+			},
+		],
+	},
+	plugins: [
+		new HTMLPlugin({
+			template: resolve('./src/index.html'),
+		}),
+		new DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+			},
+		}),
+	],
+	devtool: 'source-map',
+};
