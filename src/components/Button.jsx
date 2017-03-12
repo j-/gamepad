2664c66c7944feaf7ebe24f7db32e053d9fb28a8
j- <j@skeoh.com>
2017-03-13 01:26:41 +1100

    Improve appearance of buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,19 +7,12 @@ const ButtonValue = ({ value }) => (
 	</div>
 );
 
-const ButtonPressed = ({ pressed }) => (
-	<div>
-		{ pressed ? 'Yes' : 'No' }
-	</div>
-);
-
 const Button = ({ index, button }) => (
 	<div className={ classNames('gamepad-button', {
 		'gamepad-button-pressed': button.pressed,
 	}) }>
 		<em>Button { index }</em>
 		<ButtonValue value={ button.value } />
-		<ButtonPressed pressed={ button.pressed } />
 	</div>
 );
 
