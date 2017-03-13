4eba1525eb5b5ec3353cbeeb75f46264542067d4
j- <j@skeoh.com>
2017-03-13 23:26:08 +1100

    Add some helpful links

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,11 @@ import GamepadListContainer from './GamepadListContainer';
 const App = () => (
 	<div>
 		<h1>Gamepad</h1>
-		<p>Connect a gamepad and press any button.</p>
+		<p>Connect a gamepad and press any button to activate.</p>
+		<ul>
+			<li><a href="https://www.w3.org/TR/gamepad/">Gamepad W3C specification</a></li>
+			<li><a href="http://caniuse.com/#feat=gamepad">Can I use gamepad?</a></li>
+		</ul>
 		{ typeof navigator.getGamepads === 'function' && <GamepadListContainer /> }
 	</div>
 );
